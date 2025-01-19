dfb7416c9a2829763bfda4b6aaca17529cacec81
Frank Denis <github@pureftpd.org>
2025-01-20 00:24:18 +0100

    Remember that even getting the time requires dependencies in Rust
    When targeting wasm32-freestanding, functions from the standard
    library are defined and compile just fine, but panic at runtime...
    
    Fixes #2
    Fixes #3

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use coarsetime::Clock;
 
 fn hex_format(out: &mut [u8], bin: &[u8]) {
     const HEX_CHARS: &[u8; 16] = b"0123456789abcdef";
@@ -12,11 +12,7 @@ fn hex_format(out: &mut [u8], bin: &[u8]) {
 
 /// Return a raw UUIDv7 byte array.
 pub fn create_raw() -> [u8; 16] {
-    let start = SystemTime::now();
-    let ts = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_millis() as u64;
+    let ts = Clock::now_since_epoch().as_millis();
     let mut buf = [0u8; 16];
     buf[0..8].copy_from_slice(&(ts << 16).to_be_bytes());
 
